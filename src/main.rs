5ccf3a8496bd463745928b0e7aa189a3ec0ee7ce
Zoey Mertes <zmertes@purdue.edu>
2020-12-06 00:45:20 -0500

    Day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -16,49 +16,6 @@ use std::io::prelude::*;
 //use std::env;
 use std::convert::TryInto;
 
-#[derive(Debug, Clone, Copy)]
-struct BinaryPass {
-	fb: [char; 7],
-	lr: [char; 3]
-}
-
-impl BinaryPass {
-	fn new(input: &str) -> Option<Self> {
-		if input.len() != 10 {
-			return None;
-		}
-		let chars: Vec<char> = input.chars().collect();
-		Some(BinaryPass {
-			fb: (&chars[0..=6]).try_into().unwrap(),
-			lr: (&chars[7..=9]).try_into().unwrap()
-		})
-	}
-	fn binary_part(input: &[char], size: usize) -> usize {
-		let mut min = 0;
-		let mut max = size;
-		let mut half;
-		for c in input {
-			half = (max-min)/2;
-			if *c == 'F' || *c == 'L' {
-				max -= half;
-			} else if *c == 'B' || *c == 'R' {
-				min += half;
-			} else {
-				panic!("invalid char {}", c);
-			}
-		}
-		assert_eq!(min, max-1);
-		min
-	}
-	fn row_col(&self) -> (usize, usize) {
-		(BinaryPass::binary_part(&self.fb, 128), BinaryPass::binary_part(&self.lr, 8))
-	}
-	fn seat_id(&self) -> usize {
-		let (row, col) = self.row_col();
-		(row * 8) + col
-	}
-}
-
 fn main() -> std::io::Result<()> {
 	//let argv: Vec<String> = env::args().collect();
 	let mut input = String::new();
@@ -66,17 +23,24 @@ fn main() -> std::io::Result<()> {
 		let mut file = File::open("input.txt")?;
 		file.read_to_string(&mut input)?;
 	}
-	let mut lines: Vec<&str> = input.split('\n').collect();
-	lines.retain(|&x| x.len() != 0);
-	let passes: Vec<BinaryPass> = lines.par_iter().map(|x| BinaryPass::new(x)).flatten().collect();
-	let mut seats: Vec<usize> = passes.par_iter().map(|x| x.seat_id()).collect();
-	seats.sort();
-	let highest = seats.last().unwrap();
-	let lowest = seats.first().unwrap();
-	let sum: usize = seats.par_iter().map(|x|x-lowest).sum();
-	let n = seats.len();
-	let missing = n * (n+1)/2 - sum;
-	println!("highest: {}, lowest: {}", highest, lowest);
-	println!("missing: {}", missing+lowest);
+	//let mut lines: Vec<&str> = input.split('\n').collect();
+	let mut groups: Vec<Vec<char>> = vec![];
+	let mut temp: Vec<char> = vec![];
+	for line in input.split('\n') {
+		if line.len() == 0 {
+			groups.push(temp);
+			temp = vec![];
+		} else {
+			for c in line.chars() {
+				match temp.binary_search(&c) {
+					Ok(x) => {},
+					Err(x) => {temp.insert(x, c)}
+				}
+			}
+		}
+	}
+	let total: usize = groups.iter().map(|x| x.len()).sum();
+	println!("{} groups", total);
+	//lines.retain(|&x| x.len() != 0);
 	Ok(())
 }
