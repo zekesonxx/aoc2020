c47e92a382c8f0638f87a18645f90f2b9443a8c9
Zoey Mertes <zmertes@purdue.edu>
2020-12-06 00:59:19 -0500

    Day 6 part 2

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ extern crate rayon;
 //use nom::character::{is_digit, is_alphanumeric};
 //use nom::sequence::tuple;
 
-use rayon::prelude::*;
+//use rayon::prelude::*;
 
-
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 //use std::str;
 //use std::env;
-use std::convert::TryInto;
+//use std::convert::TryInto;
 
 fn main() -> std::io::Result<()> {
 	//let argv: Vec<String> = env::args().collect();
@@ -24,23 +24,25 @@ fn main() -> std::io::Result<()> {
 		file.read_to_string(&mut input)?;
 	}
 	//let mut lines: Vec<&str> = input.split('\n').collect();
-	let mut groups: Vec<Vec<char>> = vec![];
-	let mut temp: Vec<char> = vec![];
+	let mut total_groups = 0;
+	let mut chars: HashMap<char, usize> = HashMap::new();
+	let mut people = 0usize;
 	for line in input.split('\n') {
 		if line.len() == 0 {
-			groups.push(temp);
-			temp = vec![];
+			chars.retain(|_, v| *v == people);
+			total_groups += chars.len();
+			//reset
+			people = 0;
+			chars.retain(|_, _| false);
 		} else {
 			for c in line.chars() {
-				match temp.binary_search(&c) {
-					Ok(x) => {},
-					Err(x) => {temp.insert(x, c)}
-				}
+				let i = chars.get(&c).unwrap_or(&0).clone();
+				chars.insert(c, i+1);
 			}
+			people += 1;
 		}
 	}
-	let total: usize = groups.iter().map(|x| x.len()).sum();
-	println!("{} groups", total);
+	println!("{} groups", total_groups);
 	//lines.retain(|&x| x.len() != 0);
 	Ok(())
 }
