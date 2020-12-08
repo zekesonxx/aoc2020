dfca61c17baf3aa1b80aaaabc93ea740a9cc71c1
Zoey Mertes <zmertes@purdue.edu>
2020-12-08 00:49:08 -0500

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,6 +1,8 @@
+use rayon::prelude::*;
+
 #[derive(Debug, Clone, Copy)]
 enum Instruction {
-	Nop,
+	Nop(isize),
 	Acc(isize),
 	Jmp(isize)
 }
@@ -14,7 +16,7 @@ fn day_gen(input: &str) -> Vec<Instruction> {
 		let cmd = line[0];
 		let i = line[1].parse::<isize>().unwrap();
 		match cmd {
-			"nop" => Instruction::Nop,
+			"nop" => Instruction::Nop(i),
 			"acc" => Instruction::Acc(i),
 			"jmp" => Instruction::Jmp(i),
 			_ => unreachable!("invalid input")
@@ -29,14 +31,51 @@ fn part1(ops: &[Instruction]) -> isize {
 	let mut acc = 0;
 	while visited[ptr] != true {
 		visited[ptr] = true;
-		println!("{:?} ptr={} acc={}", ops[ptr], ptr, acc);
+		//println!("{:?} ptr={} acc={}", ops[ptr], ptr, acc);
 		use Instruction::*;
 		match ops[ptr] {
-			Nop => { ptr += 1; },
+			Nop(_) => { ptr += 1; },
 			Acc(x) => { acc += x; ptr += 1; },
 			Jmp(x) => ptr = usize::try_from(isize::try_from(ptr).unwrap() + x).unwrap()
 		}
-		println!("ptr={} acc={}", ptr, acc);
+		//println!("ptr={} acc={}", ptr, acc);
 	}
 	acc
+}
+
+fn cpu(ops: &[Instruction], swap: usize) -> Option<isize> {
+	let mut visited = vec![false; ops.len()];
+	let mut ptr = 0;
+	let mut acc = 0;
+	while ptr < ops.len() && visited[ptr] != true {
+		visited[ptr] = true;
+		use Instruction::*;
+		match ops[ptr] {
+			Nop(x) => if ptr == swap {
+				ptr = usize::try_from(isize::try_from(ptr).unwrap() + x).unwrap()
+			} else { ptr += 1; },
+			Acc(x) => { acc += x; ptr += 1; },
+			Jmp(x) => if ptr != swap {
+				ptr = usize::try_from(isize::try_from(ptr).unwrap() + x).unwrap()
+			} else { ptr += 1; },
+		}
+	}
+	if ptr==ops.len() {
+		Some(acc)
+	} else {
+		None
+	}
+}
+
+#[aoc(day8, part2)]
+fn part2(ops: &[Instruction]) -> isize {
+	ops.par_iter().enumerate().map(|x| {
+		use Instruction::*;
+		match x.1 {
+			Nop(_) | Jmp(_) => Some(x.0),
+			_ => None
+		}
+	}).flatten().find_map_any(|x| {
+		cpu(ops, x)
+	}).unwrap()
 }
